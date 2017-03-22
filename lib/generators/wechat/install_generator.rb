20b57694a4ae9eeee308a8185a66752fbdf83eb4
冯新国 <fxg@mail.com>
2017-03-22 13:47:53 +0800

    初始化脚本修改

--- lib/generators/wechat/install_generator.rb
+++ lib/generators/wechat/install_generator.rb
@@ -14,15 +14,6 @@ module Wechat
         template 'config/initializers/wechat_redis_store.rb'
       end
 
-      def add_wechat_route
-        # route 'resource :wechat, only: [:show, :create]'
-        get 'wx/:authorizer_appid/callback', to: 'wechats#show'
-        post 'wx/:authorizer_appid/callback', to: 'wechats#create'
-        get 'wx/auth', to: 'wechats#auth_callback'
-        post 'wx/auth', to: 'wechats#auth'
-        get 'wechatauth/authorize_page'
-      end
-
       def copy_wechat_controller
         template 'app/controllers/wechats_controller.rb'
       end
@@ -31,6 +22,15 @@ module Wechat
         gem 'redis'
         gem 'redis-objects'
         gem 'connection_pool'
+      end
+
+      def add_wechat_route
+        # route 'resource :wechat, only: [:show, :create]'
+        route "get 'wx/:authorizer_appid/callback', to: 'wechats#show'"
+        route  "post 'wx/:authorizer_appid/callback', to: 'wechats#create'"
+        route "get 'wx/auth', to: 'wechats#auth_callback'"
+        route "post 'wx/auth', to: 'wechats#auth'"
+        route "get 'wechatauth/authorize_page'"
       end
     end
   end
